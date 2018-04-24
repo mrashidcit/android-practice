6dfc5a941bcc2a6ed4b21628e191a2483beef5e0
Rashid Saleem <mrashidcit@gmail.com>
2018-04-24 09:45:42 +0500

    work on it later

---
+++ app/src/main/java/com/example/rashidsaleem/practice/DeveloperKey.java
@@ -0,0 +1,12 @@
+package com.example.rashidsaleem.practice;
+
+/**
+ * Created by Rashid Saleem on 3/30/2018.
+ */
+
+public class DeveloperKey {
+
+
+    public static final String DEVELOPER_KEY = "AIzaSyBdl3aOfK3jEdquBGUB9IcMEHN7j9kyEKs";
+
+}
