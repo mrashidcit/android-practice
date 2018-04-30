555b6ca4c7bdbf2d2140691d4221d25deb6e569b
Rashid Saleem <mrashidcit@gmail.com>
2018-04-30 15:02:43 +0500

    saving state

--- app/src/main/java/com/example/rashidsaleem/practice/MainActivity.java
+++ app/src/main/java/com/example/rashidsaleem/practice/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.rashidsaleem.practice;
 
 import android.Manifest;
+import android.content.ContentResolver;
 import android.content.ContentUris;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -11,172 +12,105 @@ import android.net.Uri;
 import android.os.Build;
 import android.provider.ContactsContract;
 import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String LOG_TAG = MainActivity.class.getSimpleName();
-    private static final int REQUEST_CODE_PICK_CONTACTS = 1;
-    // Request code for READ_CONTACTS. It can be any number > 0.
-    private static final int PERMISSIONS_REQUEST_READ_CONTACTS = 100;
+    private ListView mNamesListView;
 
-    private Uri uriContact;
-    private String contactID; // contacts unique ID
+    // Request code for READ_CONTACTS. It can be any number > 0.
+    private static final int PERMISSION_REQUEST_READ_CONTACTS = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        mNamesListView = (ListView) findViewById(R.id.lstNames);
 
-    public void onClickSelectContact(View btnSelectContact) {
-        // using native contacts selection
-        // Intent.ACTION_PICK = Pick an item from the data, returning what was selected.
-        startActivityForResult(new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI), REQUEST_CODE_PICK_CONTACTS);
-    }
-
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (requestCode == REQUEST_CODE_PICK_CONTACTS && resultCode == RESULT_OK) {
-            Log.d(LOG_TAG, "Response: " + data.toString());
-            uriContact = data.getData();
-
-            retrieveContactName();
-            retrieveContactNumber();
-//            retrieveContactPhoto();
-        }
+        // Read and show the contacts
+        showContacts();
 
     }
 
 
-    private void retrieveContactName() {
-
-        String contactName = null;
-
-        // querying contact data store
-        Cursor cursor = getContentResolver().query(uriContact, null, null, null, null);
-
-        if (cursor.moveToFirst()) {
-            // DISPLAY_NAME = The display name for the contact.
-            // HAS_PHONE_NUMBER =   An indicator of whether this contact has at least one phone number.
-
-            contactName = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
-
-        }
-
-        cursor.close();
-
-        Log.d(LOG_TAG, "Contact Name: " + contactName);
-
-    }
-
-
-    private void retrieveContactNumber() {
-        String contactNumber = null;
-
-        // getting contacts ID
-        Cursor cursorID = getContentResolver().query(uriContact, new String[]{ContactsContract.Contacts._ID}, null, null, null);
-
-        if (cursorID.moveToFirst()) {
-            contactID = cursorID.getString(cursorID.getColumnIndex(ContactsContract.Contacts._ID));
-
-        }
-        cursorID.close();
-
-        Log.d(LOG_TAG, "Contact ID: " + contactID);
-
+    /**
+     * Show the contacts in the ListView.
+     */
+    private void showContacts() {
         // Check the SDK version and whether the permission is already granted or not.
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
-            requestPermissions(new String[]{Manifest.permission.READ_CONTACTS}, PERMISSIONS_REQUEST_READ_CONTACTS);
+            requestPermissions(new String[]{Manifest.permission.READ_CONTACTS}, PERMISSION_REQUEST_READ_CONTACTS);
             //After this point you wait for callback in onRequestPermissionsResult(int, String[], int[]) overriden method
-
         } else {
-
-            // Using the contact ID now we will get contact phone number
-            Cursor cursorPhone = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, new String[]{ContactsContract.CommonDataKinds.Phone.NUMBER},
-
-                    ContactsContract.CommonDataKinds.Phone.CONTACT_ID + " = ? AND " +
-                            ContactsContract.CommonDataKinds.Phone.TYPE + " = " +
-                            ContactsContract.CommonDataKinds.Phone.TYPE_MOBILE,
-                    new String[]{contactID}, null);
-
-            if (cursorPhone.moveToFirst()) {
-                contactNumber = cursorPhone.getString(cursorPhone.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
-            }
-
-            cursorPhone.close();
-
-            Log.d(LOG_TAG, "Contact Phone Number: " + contactNumber);
+            // Android version is lesser than 6.0 or the permission is already granted.
+            List<String> contacts = getContactNames();
+            ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, contacts);
+            mNamesListView.setAdapter(arrayAdapter);
 
         }
 
 
-
-
     }
 
-
-    private void retrieveContactPhoto() {
-        Bitmap photo = null;
-
-        try {
-            InputStream inputStream = ContactsContract.Contacts.openContactPhotoInputStream(getContentResolver(), ContentUris.withAppendedId(ContactsContract.Contacts.CONTENT_URI, new Long(contactID)));
-
-            if (inputStream != null) {
-                photo = BitmapFactory.decodeStream(inputStream);
-                ImageView imageView = (ImageView) findViewById(R.id.img_contact);
-                imageView.setImageBitmap(photo);
-            }
-
-            assert inputStream != null;
-            inputStream.close();
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-
-    }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
 //        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-
-        if (requestCode == PERMISSIONS_REQUEST_READ_CONTACTS) {
+        if (requestCode == PERMISSION_REQUEST_READ_CONTACTS) {
             if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                // Permission is granted
-                retrieveContactNumber();
+                // Premission is granted
+                showContacts();
+
             } else {
                 Toast.makeText(this, "Until you grant the permission, we canot display the names", Toast.LENGTH_SHORT).show();
             }
+
         }
-
     }
+
+    /**
+     * Read the name of all the contacts.
+     *
+     * @return a list of names.
+     */
+    private List<String> getContactNames() {
+        List<String> contacts = new ArrayList<>();
+        // Get the ContentResolver
+        ContentResolver contentResolver = getContentResolver();
+        // Get the Cursor of all the contacts
+        Cursor cursor = contentResolver.query(ContactsContract.Contacts.CONTENT_URI, null, null, null, null);
+
+        // Move the cursor to first. Also check whether the cursor is empty or not.
+        if (cursor.moveToFirst()) {
+            // Iterate through the cursor
+            do {
+                // Get the contact name;
+                String name = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts.DISPLAY_NAME));
+
+                contacts.add(name);
+            } while (cursor.moveToNext());
+        }
+        // Close the cursor
+        cursor.close();
+
+        return contacts;
+    }
+
 }
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
---
+++ app/src/main/java/com/example/rashidsaleem/practice/util/ImageLoader.java
@@ -0,0 +1,66 @@
+package com.example.rashidsaleem.practice.util;
+
+/**
+ * Created by Rashid Saleem on 4/30/2018.
+ */
+
+/**
+ * This class wraps up completing some arbitrary long running work when loading a bitmap to an
+ * ImageView. It handles things like using a memory and disk cache, running the work in a background
+ * thread and setting a placeholder image.
+ */
+public class ImageLoader {
+   private static final String LOG_TAG = "ImageLoader";
+   private static final int FADE_IN_TIME = 200;
+
+   private ImageCache
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
